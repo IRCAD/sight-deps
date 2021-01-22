03585b4cadeab028ebe5f018aa63c6fc975f324c
￼Didier Weckmann <didier.weckmann@ircad.fr>
2021-01-22 10:48:21 +0100

    fix(build): naming test

--- cmake/utils/cpack_install.cmake
+++ cmake/utils/cpack_install.cmake
@@ -1,6 +1,6 @@
 cmake_minimum_required(VERSION 3.0)
 
 # Detects if the install is run by CPack and copies all SightDeps in CPack install folder
-if (${CMAKE_INSTALL_PREFIX} MATCHES "/_CPack_Packages/.*/(TGZ|ZIP)/")
+if (${CMAKE_INSTALL_PREFIX} MATCHES "/_CPack_Packages/.*/(TGZ|ZIP|TZST)/")
     file(COPY ${HOST_INSTALL_PATH}/ DESTINATION ${CMAKE_INSTALL_PREFIX} USE_SOURCE_PERMISSIONS)
 endif()
